9e1bf30448e0827b7dc57c53cf99b03865d9ff2f
ChrisGamerYT11202 <manosjeorjio@gmail.com>
2026-04-05 00:46:27 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "MyBot",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "discord.js": "^15.8.0",
+    "express": "^4.18.2",
+    "gtts": "^2.0.0",
+    "fs-extra": "^11.1.1"
+  }
+}
